7c99605439a3c747a3914b8c45f4b524245f0446
t-yng <t.yng.greenmonkey@gmail.com>
2025-02-13 13:23:38 -0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,15 +1,23 @@
 #!/bin/sh
 sudo dnf update -y
-echo "Start Install Apache: "
-sudo dnf install httpd -y
+
+# Install Python3
 echo ""
 echo "Start Install Python: "
-sudo dnf install python3 -y
+sudo dnf install python3.12 -y
+# Create command python3 links to python3.12
+sudo alternatives --install /usr/bin/python3 python3 /usr/bin/python3.12 1
 python3 --version
+
+# Install PHP
 echo ""
 echo "Start Install PHP: "
 sudo dnf install php php-cli php-common php-fpm php-mysqlnd -y
 php -v
+
+# Install Apache
 echo ""
+echo "Start Install Apache: "
+sudo dnf install httpd -y
 echo "Start Web Server: "
 sudo systemctl start httpd
